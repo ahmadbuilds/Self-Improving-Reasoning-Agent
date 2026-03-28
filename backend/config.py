0c27f2b2fae9fda3c04f354b6edaa2e047977cb6
Ahmad <crisitiano678@gmail.com>
2026-03-28 15:10:37 +0500

    configuration updated

--- backend/config.py
+++ backend/config.py
@@ -4,6 +4,6 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent
-load_dotenv(BASE_DIR / ".env.local")
+load_dotenv(BASE_DIR / ".env.local", override=True)
 
 groq = os.getenv("GROQ_API_KEY")
